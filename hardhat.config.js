9d8d6b599b21c7dda52c4d535a90ae3eb29e5def
vyktoremario <umehvictormario@gmail.com>
2022-01-02 02:13:04 +0100

    refactoring

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,14 @@
 // https://eth-rinkeby.alchemyapi.io/v2/RJTOkMcceoTIWbQtGStFaF0De6MIXhLC
 
 require('@nomiclabs/hardhat-waffle');
+const privateId = import.meta.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/RJTOkMcceoTIWbQtGStFaF0De6MIXhLC',
-      accounts: ['7bd6373689fe7d5dd95d34fe937769d5cdbe0461514c06e747416c1b189ed9f3']
+      accounts: []
     }
   }
 }
